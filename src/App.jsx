f75e2e4a13716404046abeb1a29b39cf4f9e0ccc
westonz7042 <quadruple.ww@gmail.com>
2025-04-10 11:54:34 -0700

    commit reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import { Outlet } from "react-router-dom";
-import Footer from ".//components/footer.js";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Sponsors from "./pages/sponsors.jsx";
@@ -17,62 +14,31 @@ import Team from "./pages/team/team.jsx";
 import Auv from "./pages/auv.jsx";
 import Electrical from "./pages/electrical.jsx";
 import Mechanical from "./pages/mechanical.jsx";
+import Footer from "./components/footer.js";
 
 function App() {
   return (
-    <div>
-      {/* <h1>Landing Page</h1> */}
-      {/* <Navbar /> */}
-
-            <Router>
-                <Navbar />
-                <Routes>
-                    <Route path="/" element={<AboutUs />} />
-                    <Route path="/about" element={<AboutUs />} />
-                    <Route path="/blog" element={<Blog />} />
-                    <Route path="/alumni" element={<Alumni />} />
-                    <Route path="/sponsors" element={<Sponsors />} />
-                    <Route path="/contact" element={<Contact />} />
-                    <Route path="/team" element={<Team />} />
-                    <Route path="/auv" element={<Auv />} />
-                    <Route path="/electrical" element={<Electrical />} />
-                    <Route path="/software" element={<Software />} />
-                    <Route path="/mechanical" element={<Mechanical />} />
-                    <Route path="/overview" element={<Overview />} />
-                </Routes>
-            </Router>
-            {/* <LandingPage /> */}
-            <main className="content-container">
-                <Outlet />
-            </main>
-            <Footer />
-        </div>
-    );
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<AboutUs />} />
-          <Route path="/about" element={<AboutUs />} />
-          <Route path="/software" element={<Software />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/alumni" element={<Alumni />} />
-          <Route path="/sponsors" element={<Sponsors />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/team" element={<Team />} />
-          <Route path="/auv" element={<Auv />} />
-          <Route path="/alumni" element={<Alumni />} />
-          <Route path="/electrical" element={<Electrical />} />
-          <Route path="/software" element={<Software />} />
-          <Route path="/mechanical" element={<Mechanical />} />
-          <Route path="/overview" element={<Overview />} />
-        </Routes>
-      </Router>
-      {/* <LandingPage /> */}
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<AboutUs />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/alumni" element={<Alumni />} />
+        <Route path="/sponsors" element={<Sponsors />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/auv" element={<Auv />} />
+        <Route path="/electrical" element={<Electrical />} />
+        <Route path="/software" element={<Software />} />
+        <Route path="/mechanical" element={<Mechanical />} />
+        <Route path="/overview" element={<Overview />} />
+      </Routes>
       <main className="content-container">
         <Outlet />
       </main>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
