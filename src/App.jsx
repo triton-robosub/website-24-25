8b5ca08d34a1113ee54102451e6c1aff44d0ce13
Nehal Choudhary <nechoudhary@ucsd.edu>
2025-05-18 15:42:59 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import Sponsors from "./pages/sponsors.jsx";
+import Home from "./pages/Home.jsx";
 import AboutUs from "./components/about.jsx";
 import { Navbar } from "./components/navbar.jsx";
+import Overview from "./pages/overview.jsx";
 import Software from "./pages/software.jsx";
 import Blog from "./pages/blog.jsx";
 import Gallery from "./components/gallery.jsx";
@@ -13,12 +17,23 @@ import Mechanical from "./pages/mechanical.jsx";
 import Footer from "./components/footer.jsx";
 // import ContactStudent from "./components/contactStudent.jsx";
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <Router basename="/website-24-25">
+      <ScrollToTop />
       <Navbar />
       <Routes>
-        <Route path="/" element={<AboutUs />} />
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<AboutUs />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/sponsors" element={<Sponsors />} />
@@ -28,6 +43,7 @@ function App() {
         <Route path="/electrical" element={<Electrical />} />
         <Route path="/software" element={<Software />} />
         <Route path="/mechanical" element={<Mechanical />} />
+        <Route path="/overview" element={<Overview />} />
         {/* <Route path="/contactStudent" element={<ContactStudent />} /> */}
       </Routes>
       <Footer />
